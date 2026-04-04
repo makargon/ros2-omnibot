508e7ada101aa947e5ceb6d7fc392d3ecc5832a0
Софья Пономарева <so02s@mail.ru>
2026-04-04 11:00:08 +0300

    check gpio

--- ros2_ws/src/omnibot_control/omnibot_control/test_pins.py
+++ ros2_ws/src/omnibot_control/omnibot_control/test_pins.py
@@ -1,18 +1,23 @@
-import board
-from adafruit_blinka.microcontroller.rp2040_u2if import pin
-from adafruit_motor import motor
+# import board
+# from adafruit_blinka.microcontroller.rp2040_u2if import pin
+# from adafruit_motor import motor
+import lgpio
+import time
 
+PIN = 0
 
-
-def main(): 
-    GP0 = pin.GP0
-    GP0.init(mode=pin.Pin.mode.OUT)
-    GP0.value = 1
-
-    pass
-    # a = 
-    # b = 
-    # motor1 = motor.DCMotor()
+def main():
+    h = lgpio.gpiod_chip(4)
+    lgpio.gpio_claim_output(h, PIN)
+    try:
+        while True:
+            lgpio.gpio_write(h, PIN, 1)
+            time.sleep(10)
+            lgpio.gpio_write(h, PIN, 0)
+            time.sleep(10)
+    except KeyboardInterrupt:
+        lgpio.gpio_write(h, PIN, 0)
+        lgpio.gpiochip_close(h)
 
 if __name__ == "main":
     main()
