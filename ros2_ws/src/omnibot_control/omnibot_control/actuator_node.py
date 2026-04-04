fd45fde3494db454b5182d44606d3604ec964eae
Софья Пономарева <so02s@mail.ru>
2026-04-04 19:33:54 +0300

    actuator node done

--- ros2_ws/src/omnibot_control/omnibot_control/actuator_node.py
+++ ros2_ws/src/omnibot_control/omnibot_control/actuator_node.py
@@ -28,7 +28,7 @@ class MotorControl:
             lgpio.gpio_write(self.chip, self.pin_a, 0)
 
     def set_speed(self, speed: float) -> None:
-        duty = int(min(1, max(0, speed)) * 65535)
+        duty = int(speed * 65535)
         self.pca.channels[self.pwm_channel].duty_cycle = duty
 
 
@@ -40,23 +40,9 @@ class ActuatorNode(Node):
         self.declare_parameter('pca_frequency', 50)
         self.declare_parameter('gpio_chip', 4)
 
-        self.declare_parameter('motor_pwm_channels', [0, 1, 2])
-        self.declare_parameter('motor_pins_a', [5, 17, 26])
-        self.declare_parameter('motor_pins_b', [6, 22, 27])
-
-        self.declare_parameter('servo_channels', [4, 5, 6])
-        self.declare_parameter('servo_actuation_range', [270, 160, 270])
-
         i2c_address = self.get_parameter('i2c_address').value
         pca_frequency = self.get_parameter('pca_frequency').value
         gpio_chip = self.get_parameter('gpio_chip').value
-
-        motor_pwm = self.get_parameter('motor_pwm_channels').get_parameter_value().integer_array_value
-        motor_a = self.get_parameter('motor_pins_a').get_parameter_value().integer_array_value
-        motor_b = self.get_parameter('motor_pins_b').get_parameter_value().integer_array_value
-
-        servo_channels = self.get_parameter('servo_channels').get_parameter_value().integer_array_value
-        servo_ranges = self.get_parameter('servo_actuation_range').get_parameter_value().integer_array_value
 
         try:
             self.pca = PCA9685(board.I2C(), address=i2c_address)
@@ -67,17 +53,24 @@ class ActuatorNode(Node):
             raise
     
         self.chip = lgpio.gpiochip_open(gpio_chip)
-        all_pins = set(motor_a) | set(motor_b)
-        for pin in all_pins:
-            lgpio.gpio_claim_output(self.chip, pin)
+        lgpio.gpio_claim_output(self.chip, 5)
+        lgpio.gpio_claim_output(self.chip, 6)
+        lgpio.gpio_claim_output(self.chip, 17)
+        lgpio.gpio_claim_output(self.chip, 22)
+        lgpio.gpio_claim_output(self.chip, 26)
+        lgpio.gpio_claim_output(self.chip, 27)
         
-        self.motors = []
-        for pwm, a, b in zip(motor_pwm, motor_a, motor_b):
-            self.motors.append(MotorControl(pwm, a, b, self.pca, self.chip))
+        self.motors = [
+            MotorControl(pwm_channel=0, pin_a=5, pin_b=6, pca=self.pca, chip=self.chip),
+            MotorControl(pwm_channel=1, pin_a=17, pin_b=22, pca=self.pca, chip=self.chip),
+            MotorControl(pwm_channel=2, pin_a=26, pin_b=27, pca=self.pca, chip=self.chip),
+        ]
 
-        self.servos = []
-        for ch, rng in zip(servo_channels, servo_ranges):
-            self.servos.append(servo.Servo(pwm_out=ch, actuation_range=rng))
+        self.servos = [
+            servo.Servo(pwm_out=self.pca.channels[4], actuation_range=160),
+            servo.Servo(pwm_out=self.pca.channels[5], actuation_range=160),
+            servo.Servo(pwm_out=self.pca.channels[6], actuation_range=160)
+        ]
 
         self.sub_motors = self.create_subscription(
             Float32MultiArray,
@@ -86,21 +79,21 @@ class ActuatorNode(Node):
             10
         )
 
-        # self.sub_servos = self.create_subscription(
-        #     Int32MultiArray,
-        #     '/servo_angles',
-        #     self.servo_callback,
-        #     10
-        # )
+        self.sub_servos = self.create_subscription(
+            Int32MultiArray,
+            '/servo_angles',
+            self.servo_callback,
+            10
+        )
 
     def motor_callback(self, msg: Float32MultiArray) -> None:
         for motor, speed in zip(self.motors, msg.data):
             motor.set_dir(speed > 0)
             motor.set_speed(abs(speed))
 
-    # def servo_callback(self, msg: Int32MultiArray) -> None:
-    #     for serv, angle in zip(self.servos, msg.data):
-    #         serv.angle = angle
+    def servo_callback(self, msg: Int32MultiArray) -> None:
+        for serv, angle in zip(self.servos, msg.data):
+            serv.angle = angle
 
     def destroy_node(self) -> bool:
         for motor in self.motors:
