b68b4a596ffed7028667305cd10fb386c67eabd6
Софья Пономарева <so02s@mail.ru>
2026-04-04 19:02:57 +0300

    actuator node done

--- ros2_ws/src/omnibot_control/omnibot_control/actuator_node.py
+++ ros2_ws/src/omnibot_control/omnibot_control/actuator_node.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 from typing import Any
-import time
 
 import rclpy
 from rclpy.node import Node
@@ -20,7 +19,7 @@ class MotorControl:
     pca: PCA9685
     chip: Any
 
-    def set_dir(self, dir: bool):
+    def set_dir(self, dir: bool) -> None:
         if dir:
             lgpio.gpio_write(self.chip, self.pin_a, 1)
             lgpio.gpio_write(self.chip, self.pin_b, 0)
@@ -28,9 +27,7 @@ class MotorControl:
             lgpio.gpio_write(self.chip, self.pin_b, 1)
             lgpio.gpio_write(self.chip, self.pin_a, 0)
 
-    def set_speed(self, speed: float):
-        # speed должен быть в [0.0, 1.0]
-        # TODO проверка значений в топике?
+    def set_speed(self, speed: float) -> None:
         duty = int(speed * 65535)
         self.pca.channels[self.pwm_channel].duty_cycle = duty
 
@@ -39,47 +36,55 @@ class ActuatorNode(Node):
     def __init__(self):
         super().__init__('actuators')
 
-        # пока без параметров
+        self.declare_parameter('i2c_address', 0x40)
+        self.declare_parameter('pca_frequency', 50)
+        self.declare_parameter('gpio_chip', 4)
+
+        self.declare_parameter('motor_pwm_channels', [0, 1, 2])
+        self.declare_parameter('motor_pins_a', [5, 17, 26])
+        self.declare_parameter('motor_pins_b', [6, 22, 27])
+
+        self.declare_parameter('servo_channels', [4, 5, 6])
+        self.declare_parameter('servo_actuation_range', [270, 160, 270])
+
+        i2c_address = self.get_parameter('i2c_address').value
+        pca_frequency = self.get_parameter('pca_frequency').value
+        gpio_chip = self.get_parameter('gpio_chip').value
+
+        motor_pwm = self.get_parameter('motor_pwm_channels').get_parameter_value().integer_array_value
+        motor_a = self.get_parameter('motor_pins_a').get_parameter_value().integer_array_value
+        motor_b = self.get_parameter('motor_pins_b').get_parameter_value().integer_array_value
+
+        servo_channels = self.get_parameter('servo_channels').get_parameter_value().integer_array_value
+        servo_ranges = self.get_parameter('servo_actuation_range').get_parameter_value().integer_array_value
+
         try:
-            self.pca = PCA9685(board.I2C(), address=0x40)
-            self.pca.frequency = 50
-            self.get_logger().info('PCA9685 успешно инициализирован по адресу 0x40')
+            self.pca = PCA9685(board.I2C(), address=i2c_address)
+            self.pca.frequency = pca_frequency
+            self.get_logger().info(f'PCA9685 успешно инициализирован по адресу {i2c_address}')
         except Exception as e:
             self.get_logger().error(f'Не удалось подключиться к PCA9685: {e}')
             raise
+    
+        self.chip = lgpio.gpiochip_open(gpio_chip)
+        all_pins = set(motor_a) | set(motor_b)
+        for pin in all_pins:
+            lgpio.gpio_claim_output(self.chip, pin)
         
-        serv = servo.Servo(self.pca.channels[6])
-        for i in range(0, 120):
-            serv.angle = i
-            print(f'angle = {i}')
-            time.sleep(0.03)
+        self.motors = []
+        for pwm, a, b in zip(motor_pwm, motor_a, motor_b):
+            self.motors.append(MotorControl(pwm, a, b, self.pca, self.chip))
 
-        self.chip = lgpio.gpiochip_open(4)
-        lgpio.gpio_claim_output(self.chip, 5)
-        lgpio.gpio_claim_output(self.chip, 6)
-        lgpio.gpio_claim_output(self.chip, 17)
-        lgpio.gpio_claim_output(self.chip, 22)
-        lgpio.gpio_claim_output(self.chip, 26)
-        lgpio.gpio_claim_output(self.chip, 27)
-        
-        self.motors = [
-            MotorControl(pwm_channel=0, pin_a=5, pin_b=6, pca=self.pca, chip=self.chip),
-            MotorControl(pwm_channel=1, pin_a=17, pin_b=22, pca=self.pca, chip=self.chip),
-            MotorControl(pwm_channel=2, pin_a=26, pin_b=27, pca=self.pca, chip=self.chip),
-        ]
+        self.servos = []
+        for ch, rng in zip(servo_channels, servo_ranges):
+            self.servos.append(servo.Servo(pwm_out=ch, actuation_range=rng))
 
-        self.servos = [
-            servo.Servo(pwm_out=self.pca.channels[4], actuation_range=160),
-            servo.Servo(pwm_out=self.pca.channels[5], actuation_range=160),
-            servo.Servo(pwm_out=self.pca.channels[6], actuation_range=160)
-        ]
-
-        # self.sub_motors = self.create_subscription(
-        #     Float32MultiArray,
-        #     '/motor_speeds',
-        #     self.motor_callback,
-        #     10
-        # )
+        self.sub_motors = self.create_subscription(
+            Float32MultiArray,
+            '/motor_speeds',
+            self.motor_callback,
+            10
+        )
 
         self.sub_servos = self.create_subscription(
             Int32MultiArray,
@@ -88,15 +93,12 @@ class ActuatorNode(Node):
             10
         )
 
-        # for motor in self.motors:
-        #     motor.set_dir(1)
-
-    def motor_callback(self, msg: Float32MultiArray):
+    def motor_callback(self, msg: Float32MultiArray) -> None:
         for motor, speed in zip(self.motors, msg.data):
             motor.set_dir(speed > 0)
             motor.set_speed(abs(speed))
 
-    def servo_callback(self, msg: Int32MultiArray):
+    def servo_callback(self, msg: Int32MultiArray) -> None:
         for serv, angle in zip(self.servos, msg.data):
             serv.angle = angle
 
