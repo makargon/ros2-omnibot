e047e72dabca2644d5e2f99aba12206bcad8dc0c
Софья Пономарева <so02s@mail.ru>
2026-04-04 13:28:54 +0300

    try test

--- ros2_ws/tests/pca_test.py
+++ ros2_ws/tests/pca_test.py
@@ -8,11 +8,11 @@ def main():
     pca = PCA9685(board.I2C(), address=0x40)
     pca.frequency = 50
     serv = servo.Servo(pca.channels[4])
-    for i in range(30, 160):
+    for i in range(30, 120):
         serv.angle = i
         time.sleep(0.03)
-    for i in range(30, 160):
-        serv.angle = 160 - i
+    for i in range(30, 120):
+        serv.angle = 120 - i
         time.sleep(0.03)
     
     pca.deinit()
