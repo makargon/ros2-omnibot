3352fa9d81e62a9a06877ccdc44f2703957feb1d
Софья Пономарева <so02s@mail.ru>
2026-04-05 16:25:33 +0300

    test encoder node

--- ros2_ws/tests/test_encoder.py
+++ ros2_ws/tests/test_encoder.py
@@ -8,22 +8,28 @@ def _on_change_b(chip, gpio, level, timestamp):
 
 chip = lgpio.gpiochip_open(4)
 
-lgpio.gpio_claim_input(chip, 10)
-lgpio.gpio_claim_input(chip, 9)
-b_a = lgpio.callback(chip, 10, func=_on_change_a)
-b_a = lgpio.callback(chip, 9, func=_on_change_b)
+pio = [10, 9, 13, 19, 20, 21]
 
-lgpio.gpio_claim_input(chip, 13)
-lgpio.gpio_claim_input(chip, 19)
-b_a = lgpio.callback(chip, 13, func=_on_change_a)
-b_a = lgpio.callback(chip, 19, func=_on_change_b)
+for pin in pio:
+    lgpio.gpio_claim_input(chip, pin)
+# lgpio.gpio_claim_input(chip, 9)
+# b_a = lgpio.callback(chip, 10, func=_on_change_a)
+# b_a = lgpio.callback(chip, 9, func=_on_change_b)
 
-lgpio.gpio_claim_input(chip, 20)
-lgpio.gpio_claim_input(chip, 21)
-b_a = lgpio.callback(chip, 20, func=_on_change_a)
-b_a = lgpio.callback(chip, 21, func=_on_change_b)
+# lgpio.gpio_claim_input(chip, 13)
+# lgpio.gpio_claim_input(chip, 19)
+# # b_a = lgpio.callback(chip, 13, func=_on_change_a)
+# # b_a = lgpio.callback(chip, 19, func=_on_change_b)
+
+# lgpio.gpio_claim_input(chip, 20)
+# lgpio.gpio_claim_input(chip, 21)
+# b_a = lgpio.callback(chip, 20, func=_on_change_a)
+# b_a = lgpio.callback(chip, 21, func=_on_change_b)
 
 
 while True:
-    pass
+    a = []
+    for pin in pio:
+        a.append(lgpio.gpio_read(chip, pin))
+    print(a)
     # print()
