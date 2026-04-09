e3f517d71d6ad1cc7c7921125bde89fcf457b12e
Софья Пономарева <so02s@mail.ru>
2026-04-09 20:12:06 +0300

    testin

--- ros2_ws/src/omnibot_kinematic/omnibot_kinematic/kinematic_node.py
+++ ros2_ws/src/omnibot_kinematic/omnibot_kinematic/kinematic_node.py
@@ -10,7 +10,7 @@ class KinematicNode(Node):
 
         self.declare_parameter('wheel_radius', 0.05)
         self.declare_parameter('wheel_base', 0.15)
-        self.declare_parameter('cmd_frame_rotation_deg', 90.0)
+        self.declare_parameter('cmd_frame_rotation_deg', 0.0)
 
         self.wheel_radius = self.get_parameter('wheel_radius').value
         self.wheel_base = self.get_parameter('wheel_base').value
@@ -33,17 +33,18 @@ class KinematicNode(Node):
 
     def cmd_vel_callback(self, msg: Twist):
         # 0
-        a = math.radians(self.cmd_frame_rotation_deg)
-        ca = math.cos(a) # 1
-        sa = math.sin(a) # 0
-        vx = ca * msg.linear.x - sa * msg.linear.y
-        vy = sa * msg.linear.x + ca * msg.linear.y
+        # a = math.radians(self.cmd_frame_rotation_deg)
+        # ca = math.cos(a) # 1
+        # sa = math.sin(a) # 0
+        vx = msg.linear.x #ca * msg.linear.x - sa * msg.linear.y
+        vy = msg.linear.y #sa * msg.linear.x + ca * msg.linear.y
         wz = msg.angular.z
 
         r = self.wheel_radius
         l = self.wheel_base
         sqrt3 = math.sqrt(3.0)
 
+        # препроверить
         w1 = (-0.5 * vx + (sqrt3 / 2.0) * vy + l * wz) / r
         w2 = (-0.5 * vx - (sqrt3 / 2.0) * vy + l * wz) / r
         w3 = (vx + l * wz) / r
