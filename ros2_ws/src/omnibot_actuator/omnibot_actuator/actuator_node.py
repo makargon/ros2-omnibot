a8d1567333a00499128feead8e9692e6ab1d2c9c
Софья Пономарева <so02s@mail.ru>
2026-04-09 19:28:46 +0300

    testin

--- ros2_ws/src/omnibot_actuator/omnibot_actuator/actuator_node.py
+++ ros2_ws/src/omnibot_actuator/omnibot_actuator/actuator_node.py
@@ -28,8 +28,8 @@ class MotorControl:
             lgpio.gpio_write(self.chip, self.pin_a, 0)
 
     def set_speed(self, speed: float) -> None:
-        duty = int(speed * 65535)
-        self.pca.channels[self.pwm_channel].duty_cycle = duty
+        # duty = int(speed * 65535)
+        self.pca.channels[self.pwm_channel].duty_cycle = speed
 
 
 class ActuatorNode(Node):
