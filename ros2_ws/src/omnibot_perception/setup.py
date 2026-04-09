0730663e1a4adba706fa812e6d764c7a17e30bc5
makarGon <makar6002@mail.ru>
2026-04-09 20:48:08 +0300

    urdf

--- ros2_ws/src/omnibot_perception/setup.py
+++ ros2_ws/src/omnibot_perception/setup.py
@@ -13,6 +13,7 @@ setup(
         (os.path.join('share', package_name), ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.py'))),
         (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.urdf'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
