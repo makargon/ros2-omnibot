0edb7b34ab2b2c3192968ffde87414be691ead30
Софья Пономарева <so02s@mail.ru>
2026-04-10 14:30:12 +0300

    testing pid

--- ros2_ws/src/omnibot_pid_wheel/src/pid_wheel_node.cpp
+++ ros2_ws/src/omnibot_pid_wheel/src/pid_wheel_node.cpp
@@ -81,7 +81,7 @@ void PIDWheelNode::timer_callback()
 
     for (size_t i = 0; i < NUM_WHEELS; ++i) {
         pid_controllers_[i].T = dt;
-        motor_msg.data[i] = 0 if setpoint_[i] == 0 else PIDController_Update(
+        motor_msg.data[i] = setpoint_[i] == 0 ? 0 : PIDController_Update(
             &pid_controllers_[i],
             setpoint_[i],
             measurement_[i]);
