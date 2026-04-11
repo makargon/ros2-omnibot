306b2e4fc75f66dd1bae6338883efaf8085b8ec6
makarGon <makar6002@mail.ru>
2026-04-11 16:32:36 +0300

    demo

--- ros2_ws/src/omnibot_bringup/omnibot_bringup/demo.py
+++ ros2_ws/src/omnibot_bringup/omnibot_bringup/demo.py
@@ -1,42 +1,40 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 
 class DemoNode(Node):
     def __init__(self):
         super().__init__('demo_node')
         self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
-        loop_rate = 0.5  # Hz
-        self._loop_rate = self.create_rate(loop_rate, self.get_clock())
-        self.timer = self.create_timer(3.0, self.timer_callback)
+
+        # Шаги движения по квадрату: вперед, влево, назад, вправо
+        self._steps = [
+            (0.1, 0.0),
+            (0.0, 0.1),
+            (-0.1, 0.0),
+            (0.0, -0.1),
+        ]
+        self._step_idx = 0
+
+        # Публикуем новую команду раз в секунду без блокирующего sleep внутри callback.
+        self.timer = self.create_timer(1.0, self.timer_callback)
         self.get_logger().info('Demo node started')
 
     def timer_callback(self):
-        """Бесконечно двигается по квадрату раз в секунду"""
-        self.get_logger().info('Start')
+        """Бесконечно двигается по квадрату, меняя направление раз в секунду."""
+        vx, vy = self._steps[self._step_idx]
+
         msg = Twist()
+        msg.linear.x = vx
+        msg.linear.y = vy
         msg.angular.z = 0.0
-        msg.linear.x = 0.1
-        msg.linear.y = 0.0
         self.publisher.publish(msg)
-        self.get_logger().info('Published cmd_vel: linear.x=%.2f angular.z=%.2f' % (msg.linear.x, msg.angular.z))
-        self._loop_rate.sleep()
-        msg.linear.x = 0.0
-        msg.linear.y = 0.1
-        self.publisher.publish(msg)
-        self.get_logger().info('Published cmd_vel: linear.x=%.2f linear.y=%.2f' % (msg.linear.x, msg.linear.y))
-        self._loop_rate.sleep()
-        msg.linear.x = -0.1
-        msg.linear.y = 0.0
-        self.publisher.publish(msg)
-        self.get_logger().info('Published cmd_vel: linear.x=%.2f angular.z=%.2f' % (msg.linear.x, msg.angular.z))
-        self._loop_rate.sleep()
-        msg.linear.x = 0.0
-        msg.linear.y = -0.1
-        self.publisher.publish(msg)
-        self.get_logger().info('Published cmd_vel: linear.y=%.2f' % (msg.linear.y))
-        self._loop_rate.sleep()
+        self.get_logger().info(
+            'Published cmd_vel: linear.x=%.2f linear.y=%.2f angular.z=%.2f'
+            % (msg.linear.x, msg.linear.y, msg.angular.z)
+        )
+
+        self._step_idx = (self._step_idx + 1) % len(self._steps)
 
 
 def main(args=None):
