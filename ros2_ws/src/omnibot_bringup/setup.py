c5877a190aa0297dc2044b57e200ea750c21963d
Софья Пономарева <so02s@mail.ru>
2026-04-11 15:12:22 +0300

    arm

--- ros2_ws/src/omnibot_bringup/setup.py
+++ ros2_ws/src/omnibot_bringup/setup.py
@@ -12,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         (os.path.join('share', package_name), ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
-        # (os.path.join('share', package_name, 'missions'), glob(os.path.join('missions', '*.py'))),
+        (os.path.join('share', package_name, 'missions'), glob(os.path.join('missions', '*.py'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
